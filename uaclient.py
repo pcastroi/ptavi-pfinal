84d20bddbf84b5681abe3b87d4385eb3f3229062
Pablo Castro Ilundain <pilundai@gamma08.aulas.gsyc.urjc.es>
2017-12-11 11:04:31 +0100

    cliente mejorado

--- uaclient.py
+++ uaclient.py
@@ -1,33 +1,96 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa User Agent Client
 """
 
 import socket
 import sys
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
-# Compruebo los argumentos de entrada
-if len(sys.argv) != 3:
-    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-# Contenido que vamos a enviar
-METHOD = sys.argv[1]
-MLOGIN = sys.argv[2][:sys.argv[2].find('@')]
-MSERVER = sys.argv[2][sys.argv[2].find('@') + 1: sys.argv[2].find(':')]
-MPORT = sys.argv[2][sys.argv[2].rfind(':') + 1:]
+#Creamos una clase para leer el contenido del fichero de configuración xml
+class XMLHandler(ContentHandler):
+
+    def __init__(self):
+        """
+        Constructor. Inicializamos las variables
+        """
+        self.list_tags = []
+        self.tags = {'account': ['username', 'passwd'],
+                     'uaserver': ['ip', 'puerto'],
+                     'rtpaudio': 'puerto',
+                     'regproxy': ['ip', 'puerto'],
+                     'log': 'path',
+                     'audio': 'path'}
+
+    def startElement(self, name, attrs):
+        """
+        Método de inicio
+        """
+        print("a")
+        diccionario = {}
+        if name in self.tags:
+            print("B")
+            diccionario['tag'] = name
+            for elem in self.tags[name]:
+                diccionario[elem] = attrs.get(elem, "")
+            self.list_tags.append(diccionario)
+
+    def get_tags(self):
+        """
+        Devuelve la lista
+        """
+        return self.list_tags
+        
+    def __str__(self):
+        """
+        Devuelve una cadena de texto con todas las etiquetas y atributos
+        """
+        listcad = []
+        for dic in self.list_tags:
+            listcad.append(dic['tag'])
+            for atribs in dic:
+                if atribs != 'tag' and dic[atribs]:
+                    listcad.append('\t' + atribs + '=' + dic[atribs] + '\t')
+            listcad.append('\n')
+        return(''.join(listcad))
+
+if __name__ == '__main__':
+    # Compruebo los argumentos de entrada
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python uaclient.py config method option")
+
+    # Argumentos que me pasan como parámetros
+    CONFIG = sys.argv[1]
+    METHOD = sys.argv[2]
+    OPTION = sys.argv[3]
+
+
+    parser = make_parser()
+    listxml = XMLHandler()
+    parser.setContentHandler(listxml)
+    print(CONFIG)
+    parser.parse(open(CONFIG))
+    print(listxml.__str__())
+    
+
+
+
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((MSERVER, int(MPORT)))
+#with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 
-    Msend = 'sip:' + MLOGIN + '@' + MSERVER + ' SIP/2.0\r\n'
-    my_socket.send(bytes(METHOD + ' ' + Msend, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print(data.decode('utf-8'))
+#    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#    my_socket.connect((MSERVER, int(MPORT)))
 
-    if data.decode('utf-8').split()[1] == "100":
-        my_socket.send(bytes("ACK" + ' ' + Msend, 'utf-8') + b'\r\n')
-    print("Conection finished.")
+#    Msend = 'sip:' + MLOGIN + '@' + MSERVER + ' SIP/2.0\r\n'
+#    my_socket.send(bytes(METHOD + ' ' + Msend, 'utf-8') + b'\r\n')
+#    data = my_socket.recv(1024)
+#    print(data.decode('utf-8'))
+
+#    if data.decode('utf-8').split()[1] == "100":
+#        my_socket.send(bytes("ACK" + ' ' + Msend, 'utf-8') + b'\r\n')
+#    print("Conection finished.")
